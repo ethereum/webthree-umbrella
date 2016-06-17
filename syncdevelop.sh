fa0870a396cf292c2ea6b0e6539af247f7282daa
Dimitry <winsvega@mail.ru>
2016-06-17 17:06:09 +0300

    test2

---
+++ syncdevelop.sh
@@ -0,0 +1,56 @@
+echo libethereum
+cd libethereum
+git fetch origin
+git checkout develop
+git pull
+cd ..
+
+echo webthree
+cd webthree
+git fetch origin
+git checkout develop
+git pull
+cd ..
+
+echo mix
+cd mix
+git fetch origin
+git checkout develop
+git pull
+cd ..
+
+echo webthree-helpers
+cd webthree-helpers
+git fetch origin
+git checkout develop
+git pull
+cd ..
+
+echo libweb3core 
+cd libweb3core
+git fetch origin
+git checkout develop
+git pull
+cd ..
+
+echo alethzero
+cd alethzero
+git fetch origin
+git checkout develop
+git pull
+cd ..
+
+echo solidity
+cd solidity
+git fetch origin
+git checkout develop
+git pull
+cd ..
+
+echo web3.js
+cd web3.js
+git fetch origin
+git checkout develop
+git pull
+cd ..
+
